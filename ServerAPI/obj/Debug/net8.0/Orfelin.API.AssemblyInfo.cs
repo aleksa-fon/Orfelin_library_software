7bce549be14e95578164225f02f0d190df74ba92
Aleksa Jevtić <aleksa.jevtic@fon.bg.ac.yu>
2026-06-11 17:27:06 +0200

    Preimenovan solution

--- ServerAPI/obj/Debug/net8.0/Orfelin.API.AssemblyInfo.cs
+++ ServerAPI/obj/Debug/net8.0/Orfelin.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orfelin.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6d0fb4ac84c610652fbb422ef19bd83b22cc39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+176aeb6458cf2efe629b9c2ff2e5518c9786d8b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orfelin.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orfelin.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
